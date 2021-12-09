8c6940b13b5c90d0088d4c185d91375454404f23
RooSoft <marc@roosoft.com>
2021-12-09 14:16:25 -0500

    halve channel count and total capacity because all channels have been doubled by bi-directionality

--- lib/neo4j/aggregate.ex
+++ lib/neo4j/aggregate.ex
@@ -7,7 +7,7 @@ defmodule LnImport.Neo4j.Aggregate do
     query = """
     MATCH (n:node)-[c:CHANNEL]-()
     WITH n, count(c) as channel_count
-    SET n.channel_count = channel_count
+    SET n.channel_count = channel_count/2
     """
 
     %Bolt.Sips.Response{stats: %{"properties-set" => nb_nodes}} = Bolt.Sips.query!(conn, query)
@@ -23,7 +23,7 @@ defmodule LnImport.Neo4j.Aggregate do
     query = """
     MATCH (n:node)-[c:CHANNEL]-(:node)
     WITH n, sum(c.capacity) as node_capacity
-    SET n.total_capacity = node_capacity
+    SET n.total_capacity = node_capacity/2
     """
 
     %Bolt.Sips.Response{stats: %{"properties-set" => nb_nodes}} = Bolt.Sips.query!(conn, query)
