46b27e9873bf23397b74d71baf7a24c2695ed732
RooSoft <marc@roosoft.com>
2021-12-10 09:31:41 -0500

    corrected a namespace problem

--- lib/commands/convert.ex
+++ lib/commands/convert.ex
@@ -7,7 +7,7 @@ defmodule Lnq.Commands.Convert do
   @nodes_csv_file_path "#{@neo4j_import_folder}#{@nodes_csv_filename}"
   @channels_csv_file_path "#{@neo4j_import_folder}#{@channels_csv_filename}"
 
-  alias LightningGraph.FileConverter
+  alias LightningGraph.Lnd.FileConverter
 
   def get_definition do
     [
