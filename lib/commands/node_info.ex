c3c5d9ea21b5d40b32fdb496d7a146f1e11452f3
RooSoft <marc@roosoft.com>
2022-01-24 09:05:33 -0500

    added node-info command

---
+++ lib/commands/node_info.ex
@@ -0,0 +1,25 @@
+defmodule Lnq.Commands.NodeInfo do
+  alias LightningGraph.Neo4j
+  alias Lnq.Formatting
+
+  def get_definition do
+    [
+      name: "node-info",
+      about: "Display basic node info",
+      args: [
+        node_alias: [
+          value_name: "ALIAS",
+          help: "Node's alias",
+          required: true,
+          parser: :string
+        ]
+      ]
+    ]
+  end
+
+  def execute(node_alias) do
+    Neo4j.get_connection()
+    |> Neo4j.Query.get_node_by_alias(node_alias)
+    |> Formatting.Query.get_node_by_alias()
+  end
+end
